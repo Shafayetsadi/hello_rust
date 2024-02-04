58fa21d65a42ed1f76b86e8d516c61cffd67ea0e
Shafayetul Huda Sadi <shafayet.sadi@gmail.com>
2024-02-04 20:45:07 +0600

    added Rng

--- The-Rust-Programming-Language/guessing_game/src/main.rs
+++ The-Rust-Programming-Language/guessing_game/src/main.rs
@@ -1,16 +1,21 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, rust!");
-
     println!("Guess the number!");
+
+    let secret_number: i32 = rand::thread_rng()
+        .gen_range(1..=10); // ..= -> inclusive range
+
     println!("Enter your guess: ");
 
-    let mut guess = String::new(); // mutable variable with new instance of String
+    let mut guess: String = String::new(); // mutable variable with new instance of String
 
     io::stdin()
         .read_line(&mut guess) // read and store in guess, &mut -> mutable reference
         .expect("Failed too read line"); // handling error
 
+    println!("The secret number is: {}", secret_number);
     println!("You guessed: {}", guess);
+
 }
