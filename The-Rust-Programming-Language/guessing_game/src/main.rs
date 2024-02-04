1f68b49984a65840dc8787ec916f44aa4b1a635b
Shafayetul Huda Sadi <shafayet.sadi@gmail.com>
2024-02-04 21:03:47 +0600

    added gameloop

--- The-Rust-Programming-Language/guessing_game/src/main.rs
+++ The-Rust-Programming-Language/guessing_game/src/main.rs
@@ -8,26 +8,35 @@ fn main() {
     let secret_number: i32 = rand::thread_rng()
         .gen_range(1..=10); // ..= -> inclusive range
 
-    println!("Enter your guess: ");
+    loop {
+        println!("Enter your guess: ");
 
-    let mut guess: String = String::new(); // mutable variable with new instance of String
+        let mut guess: String = String::new(); // mutable variable with new instance of String
 
-    io::stdin()
-        .read_line(&mut guess) // read and store in guess, &mut -> mutable reference
-        .expect("Failed too read line"); // handling error
+        io::stdin()
+            .read_line(&mut guess) // read and store in guess, &mut -> mutable reference
+            .expect("Failed too read line"); // handling error
+/*
+        let guess: i32 = guess
+            .trim() // remove whitespace
+            .parse() // parse string to integer
+            .expect("Please type a number!"); // handling error
+*/
+        let guess:i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: i32 = guess
-        .trim() // remove whitespace
-        .parse() // parse string to integer
-        .expect("Please type a number!"); // handling error
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => { println!("Too small!") }
-        Ordering::Equal => { println!("You got it!") }
-        Ordering::Greater => { println!("Too big!") }
+        match guess.cmp(&secret_number) {
+            Ordering::Less => { println!("Too small!") }
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+            Ordering::Greater => { println!("Too big!") }
+        }
     }
 
     println!("The secret number is: {}", secret_number);
-    println!("You guessed: {}", guess);
 
 }
