e8a169c11e5f03d5d527aa5d7bcbf7aba9721bfa
Shafayetul Huda Sadi <shafayet.sadi@gmail.com>
2024-02-05 19:36:09 +0600

    fixed index

--- Rust-Book/common_concepts/src/main.rs
+++ Rust-Book/common_concepts/src/main.rs
@@ -138,11 +138,11 @@ fn main() {
     // loop with a label
     'outer: loop {
         println!("Entered the outer loop");
-        'inner: loop {
+        loop {
             println!("Entered the inner loop");
             break 'outer; // break the outer loop
         }
-        println!("This point will never be reached");
+        // println!("This point will never be reached");
     }
     let mut number: i32 = 0;
     while number < 5 {
